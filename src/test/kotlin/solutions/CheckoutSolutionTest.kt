1a0cf16e26690920de0e6d5a87252d0f777e8e61
sbx_user1051 <sbx_user1051@169.254.244.13>
2023-09-12 20:13:33 +0000

    Tue Sep 12 20:13:33 UTC 2023

--- src/test/kotlin/solutions/CheckoutSolutionTest.kt
+++ src/test/kotlin/solutions/CheckoutSolutionTest.kt
@@ -13,10 +13,11 @@ class CheckoutSolutionTest {
         assertEquals(80, CheckoutSolution.checkout("AB"))
         assertEquals(95, CheckoutSolution.checkout("ABD"))
         assertEquals(130, CheckoutSolution.checkout("AAA"))
-        assertEquals(180, CheckoutSolution.checkout("AAAB"))
+        assertEquals(160, CheckoutSolution.checkout("AAAB"))
         assertEquals(230, CheckoutSolution.checkout("AAAAA"))
         assertEquals(-1, CheckoutSolution.checkout("E"))
         assertEquals(-1, CheckoutSolution.checkout("AE"))
 
     }
 }
+
