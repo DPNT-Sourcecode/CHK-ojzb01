7d922252a13959fd00b676e0c1af58daf716e3f8
sbx_user1051 <sbx_user1051@169.254.244.13>
2023-09-12 22:23:21 +0000

    Tue Sep 12 22:23:21 UTC 2023

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -76,7 +76,14 @@ object CheckoutSolution {
     private fun processGroupDiscounts(itemCounts: MutableMap<Char, Int>): Int {
         val total = 0
         productMap.values.forEach { product ->
-            
+            val groupOffers = product.offers.filter { it.groupItems != null }
+            groupOffers.forEach { offer ->
+                val eligibleGroupItems = offer.groupItems!!.associateWith {
+                    itemCounts.getOrDefault(it, 0)
+                }.toMutableMap()
+                while (eligibleGroupItems.values.sum() >= offer.groupItems)
+            }
+
         }
     }
 
@@ -98,6 +105,7 @@ object CheckoutSolution {
         return total
     }
 }
+
 
 
 
