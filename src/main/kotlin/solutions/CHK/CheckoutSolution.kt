b0c093790c9d001f269be83df9f794eea753e780
sbx_user1051 <sbx_user1051@169.254.244.13>
2023-09-12 21:04:07 +0000

    Tue Sep 12 21:04:07 UTC 2023

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -9,31 +9,29 @@ object CheckoutSolution {
         'B' to Product(30, listOf(Offer(2, 45))),
         'C' to Product(20),
         'D' to Product(15),
-        'E' to Product(40),
-        'F' to Product(10),
+        'E' to Product(40, listOf(Offer(2, 80, 'B'))),
+        'F' to Product(10, listOf(Offer(3, 20))),
         'G' to Product(20),
-        'H' to Product(10),
+        'H' to Product(10, listOf(Offer(10, 80), Offer(5, 45))),
         'I' to Product(35),
         'J' to Product(60),
-        'K' to Product(80),
+        'K' to Product(80, listOf(Offer(2, 150))),
         'L' to Product(90),
         'M' to Product(15),
-        'N' to Product(40),
+        'N' to Product(40, listOf(Offer(3, 120, 'M'))),
         'O' to Product(10),
-        'P' to Product(50),
-        'Q' to Product(30),
-        'R' to Product(50),
+        'P' to Product(50, listOf(Offer(5, 200))),
+        'Q' to Product(30, listOf(Offer(3, 80))),
+        'R' to Product(50, listOf(Offer(3, 150, 'Q'))),
         'S' to Product(30),
         'T' to Product(20),
-        'U' to Product(40),
-        'V' to Product(50),
+        'U' to Product(40, listOf(Offer(4,120))),
+        'V' to Product(50, listOf(Offer(3, 130), Offer(2, 90))),
         'W' to Product(20),
         'X' to Product(90),
         'Y' to Product(10),
-        'Z' to Product(50),
-
-
-        )
+        'Z' to Product(50)
+    )
 
     fun checkout(skus: String): Int {
         if (skus.any { it !in "ABCDEF" }) return -1
@@ -88,6 +86,7 @@ object CheckoutSolution {
         return offer * 45 + remaining * 30
     }
 }
+
 
 
 
