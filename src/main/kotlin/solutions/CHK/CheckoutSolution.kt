94e233b1261fa08f8fc7cd1cc5a9660d4af7ec35
sbx_user1051 <sbx_user1051@169.254.244.13>
2023-09-12 20:01:15 +0000

    Tue Sep 12 20:01:15 UTC 2023

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -2,6 +2,13 @@ package solutions.CHK
 
 object CheckoutSolution {
     fun checkout(skus: String): Int {
-        
+        if (skus.any { it !in "ABCD" }) return -1
+
+        val itemCounts = mapOf<Char, Int>(
+            'A' to 0,
+            'B' to 0,
+            'C' to 0,
+            'D' to 0
+        )
     }
 }
